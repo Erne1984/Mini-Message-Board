00c91e89f6631179bf9f8f924f9db80dc5a2ebb2
Ernesto <ernestofamorim@gmail.com>
2024-06-05 10:23:57 -0300

    adding msgs

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,36 @@
 const express = require("express");
 
-const router = express.Router();
+const routes = express.Router();
 
 const path = require("path");
 
-router.get("/", (req, res, next) => {
-    res.sendFile(path.join)
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+routes.get("/", (req, res, next) => {
+  res.render("index", { messages });
+  //res.sendFile(path.join)
 })
+
+routes.post("/new", (req, res, next) => {
+  const msg = req.body.msg;
+  const author = req.body.author;
+
+  messages.push({ text: msg, user: author, added: new Date() });
+
+  res.redirect("/")
+
+
+})
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const routes = require("./routes/routes");
+const path = require("path")
+
+const app = express();
+
+const port = process.env.port || 8081
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.urlencoded())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes); 
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`localhost:${port}/`);
+});
